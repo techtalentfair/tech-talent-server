63fa2a66312e504d6ca7f2688cca57afcf77a71e
YousseefAh <yousseef.ah@gmail.com>
2025-03-08 00:12:07 +0200

    pushing first

---
+++ controllers/aboutPageController.js
@@ -0,0 +1,27 @@
+import { AboutPageModel } from "../models/aboutPage.model";
+
+// Get About Page Data
+export const getAboutPage = async (req, res) => {
+  try {
+    const aboutPage = await AboutPageModel.findOne();
+    res.json(aboutPage);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+// Update About Page Data (Protected Route)
+export const updateAboutPage = async (req, res) => {
+  try {
+    let aboutPage = await AboutPageModel.findOne();
+    if (!aboutPage) {
+      aboutPage = new AboutPageModel(req.body);
+    } else {
+      Object.assign(aboutPage, req.body);
+    }
+    await aboutPage.save();
+    res.json({ message: "Updated successfully", data: aboutPage });
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
---
+++ models/aboutPage.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const CardSchema = new Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  image: { type: String, required: true },
+});
+
+const AboutPageSchema = new Schema({
+  landingSection: {
+    landingTitle: { type: String, required: true },
+    landingSubTitle: { type: String, required: true },
+    landingDescription: { type: String, required: true },
+    landingImage: { type: String, required: true },
+    landingVideo: { type: String, required: true },
+  },
+  meetOurTeam: {
+    mentorCard: { type: CardSchema, required: true },
+    headTeams: { type: [CardSchema], required: true },
+  },
+});
+
+export const AboutPageModel = mongoose.model("AboutPage", AboutPageSchema);
